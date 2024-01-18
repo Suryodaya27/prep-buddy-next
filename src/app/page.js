b13c0714c57535812d016b7b2c2af5db9aa540ca
Suryodaya pandey <79349719+Suryodaya27@users.noreply.github.com>
2024-01-18 20:53:54 +0530

    added navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
 import McqInput from "@/components/McqInput"
+import { MainNav } from "@/components/Navbar"
+
 export default function Home() {
   return (
     <div>
+      <MainNav/>
       <McqInput/>
     </div>
   )
