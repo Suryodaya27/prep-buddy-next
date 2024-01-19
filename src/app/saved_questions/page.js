37595ff5909737f80a394ae0d9812c76fd478276
Suryodaya pandey <79349719+Suryodaya27@users.noreply.github.com>
2024-01-19 11:53:09 +0530

    drawer added

--- src/app/saved_questions/page.js
+++ src/app/saved_questions/page.js
@@ -1,10 +1,39 @@
 import { MainNav } from "@/components/Navbar";
+import draw from "@/components/draw";
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
+import { Button } from "@/components/ui/button";
 
 export default function SavedQuestion() {
   return (
     <div className="flex flex-col">
         <MainNav/>
     <h1 className="text-xl text-center mt-5">Feature soon to be added ...</h1>
+    <Drawer>
+        <DrawerTrigger>Previous inputs</DrawerTrigger>
+        <DrawerContent className="h-[400px]">
+          <DrawerHeader>
+            <DrawerTitle>Previously input texts</DrawerTitle>
+            <DrawerDescription>All Previously used inputs will appear here...</DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter className="mb-10">
+            {/* <Button className="w-[100px] m-auto">Submit</Button> */}
+            <DrawerClose>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </div>
   )
 }
