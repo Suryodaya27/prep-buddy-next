3831b7b6922716e686a43cbc807a3e306ad4cda8
Suryodaya pandey <79349719+Suryodaya27@users.noreply.github.com>
2024-01-29 19:43:14 +0530

    added request for palm

--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,8 @@
 import { NextResponse} from 'next/server';
 import cookie from 'cookie'; // you might need to install the 'cookie' package
 
-// const REDIRECT_PATH =  "https://prep-buddy-next.vercel.app/auth";
-const REDIRECT_PATH =  "http://localhost:3000/auth";
+const REDIRECT_PATH =  "https://prep-buddy-next.vercel.app/auth";
+// const REDIRECT_PATH =  "http://localhost:3000/auth";
 export function middleware(req) {
     const token = req.cookies.get("token")
 
