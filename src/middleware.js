7b6dfb8762aa016e44f389af62af5eec7ffe5d7b
Suryodaya pandey <79349719+Suryodaya27@users.noreply.github.com>
2024-03-04 18:09:25 +0530

    deleted unnecessary files

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,7 @@
 import { NextResponse} from 'next/server';
 
-const REDIRECT_PATH =  "https://prep-buddy-next.vercel.app/auth";
-// const REDIRECT_PATH =  "http://localhost:3000/auth";
+// const REDIRECT_PATH =  "https://prep-buddy-next.vercel.app/auth";
+const REDIRECT_PATH =  "http://localhost:3000/auth";
 export function middleware(req) {
     const token = req.cookies.get("token")
 
